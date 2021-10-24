cc489571b359fb03b8e65c6c806e9102e14169ea
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-24 20:00:44 +0530

    complete local image storage

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,7 +1,34 @@
 const { StatusCodes } = require('http-status-codes')
+const path = require('path')
+const CustomErrors = require('../errors')
 
 const uploadProductImage = async (req, res) => {
-  res.send('upload product image')
+  if (!req.files) {
+    throw new CustomErrors.BadRequestError('No File uploaded')
+  }
+
+  const productImage = req.files.image
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomErrors.BadRequestError('Please upload image')
+  }
+
+  const maxSize = 5 * 1024 * 1024
+  if (productImage.size > maxSize) {
+    throw new CustomErrors.BadRequestError(
+      `Please upload image smaller than 5MB`
+    )
+  }
+
+  const imagePath = path.join(
+    __dirname,
+    '../public/uploads/' + `${productImage.name}`
+  )
+
+  await productImage.mv(imagePath)
+
+  res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } })
 }
 
 module.exports = { uploadProductImage }
